4ec34010770cd86484cb1b5013ef463226062bb9
Daniel Roque <P0654608@portoseguro.com.br>
2025-06-02 23:40:54 -0300

    Adicionando a Listagem de Pacientes, e adicionando o ID de medicos no retorno da listagem do mesmo

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,17 +1,26 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DadosCadastroPaciente;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("ados recebidos"+dados);
+        System.out.println("ados recebidos" + dados);
+    }
+
+    @GetMapping
+    public Page<DadosCadastroPaciente> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosCadastroPaciente::new);
     }
 }
